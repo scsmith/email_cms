306d590c8b8ad901a58a6c7fffd500fbb4768ccf
Steve Smith <github@scsworld.co.uk>
2010-08-25 16:12:16 +0100

    Lets add the ability to parse and understand multipart messages and store all attachments in gridfs and simply call it as an image (we could check for images or other types later)

--- email_cms.rb
+++ email_cms.rb
@@ -15,17 +15,52 @@ get '/:id' do
   haml :post
 end
 
+get '/delete' do
+  Post.destroy_all
+  'Deleted all posts'
+end
+
 post '/incoming_message/' do
   message = Mail.new(params[:message])
   
-  @post = Post.create!(:title => message.subject, :body => message.body.decoded)
+  if message.multipart?
+    attachments = []
+    body = ""
+    
+    message.parts.each do |part|
+      if part.content_type =~ /^text\/plain/i
+        body << part.body.decoded
+      elsif part.attachment?
+        @grid ||= Mongo::Grid::new(Mongoid.database)
+        file = @grid.put(part.body.decoded)
+        attachments << Attachment.new(:grid_id => file)
+      end
+    end
+    
+    @post = Post.create!(:title => message.subject, :body => body, :attachments => attachments)
+  else
+    @post = Post.create!(:title => message.subject, :body => message.body.decoded)
+  end
+  
   haml :post
+end
+
+get '/images/:id' do
+  grid = Mongo::Grid::new(Mongoid.database)
+  grid.get(BSON::ObjectID.from_string(params[:id])).read
 end
 
 class Post
   include Mongoid::Document
   field :title
   field :body
+  embeds_many :attachments
+end
+
+class Attachment
+  include Mongoid::Document
+  
+  field :grid_id
 end
 
 __END__
@@ -45,7 +80,8 @@ __END__
         %a{:href => "/#{post.id}"}= post.title
       #content
         = post.body
-    
+        - for attachment in post.attachments do
+          %img{:src => "/images/#{attachment.grid_id}"}
 @@post
 %h1= @post.title
 #content
